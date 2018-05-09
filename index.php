b8662eddc7b9efabc508cf49c1f12c5dd3a1a9e4
avivahds <avivah.sisfo@student.untan.ac.id>
2018-05-09 20:08:37 +0700

    Update ni pak

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php 
+	//ini merupakan tester
+	// walau sepanjang apapun komentar yang tertulis disini, ia tak akan pernah dianggap
 echo "Percobaan sajalah";
 echo "Nama saya Avivah";
- ?>
+
+	$angka = "satu";
+	$lagi = "satu";
+	$kata = 2;
+
+	echo $angka.$kata.$lagi. "<br>";
+	echo "Berat badan anak saya adalah ".$kata." kg";
